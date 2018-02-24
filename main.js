3b5017b21f15474acfd8e02e57626b8cbbf9b126
kamiya <psoc5lp@gmail.com>
2018-02-24 16:30:19 +0900

    運行情報を取得可能

--- main.js
+++ main.js
@@ -12,13 +12,56 @@ const cheerio = require("cheerio");
     'http://traininfo.jreast.co.jp/train_info/shinkansen.aspx',
     'http://traininfo.jreast.co.jp/train_info/chyokyori.aspx',
   ];
+  const result = [];
   for(url of targetUrls){
     console.log('goto', url);
     await page.goto(url);
 
-    const names = await page.$$eval('.text-tit-xlarge', list => list.map(data => data.textContent));
-    const statuses = await page.$$eval('td > img', list => list.map(data => data.alt));
-    console.log(names, statuses);
+    const entries = await page.evaluate(() => {
+        const trs = document.getElementsByTagName('tr');
+        const arr = [];
+        //最後に処理したデータが遅延情報持ちならこっちに入れる
+        let delayData = null;
+
+        for(let i = 0 ; i < trs.length ; ++i) {
+          const tr = trs[i];
+
+          if(delayData) {
+            //遅延データも入れて返す
+            arr.push({
+              name: delayData.tr.children[0].textContent,
+              status: delayData.tr.children[1].children[0].alt,
+              time: delayData.tr.children[2].textContent,
+              message: tr.children[0].textContent,// 遅延情報の行はこれだけ
+              hasHistory: delayData.hasHistory,
+            });
+            delayData = null;
+          } else {
+            const hasTime = tr.getElementsByClassName('time').length > 0;
+            // 遅延情報に履歴があればこっちのいずれか
+            const hasHistory = tr.getElementsByClassName('history').length > 0;
+            const hasTime02 = tr.getElementsByClassName('time02').length > 0;
+            if(hasHistory | hasTime | hasTime02) {
+              delayData = {
+                tr: tr,
+                hasHistory: hasHistory,
+              };
+              continue;
+            } else {
+              arr.push({
+                name: tr.children[0].textContent,
+                status: tr.children[1].children[0].alt,
+              });
+            }
+          }
+        }
+        return arr;
+      }
+    );
+    entries.forEach(v => result.push(v));
   }
+  result.forEach(v => {
+    console.log(v);
+  });
   await browser.close();
 })();
